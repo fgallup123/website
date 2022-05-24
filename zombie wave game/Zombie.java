6b931bb5ce8f243308d82293a99884a0cbde284b
p7gallup24 <89165529+p7gallup24@users.noreply.github.com>
2022-05-24 15:02:13 -0700

    Add files via upload

---
+++ zombie wave game/Zombie.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class zombie here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Zombie extends Actor
+{
+    /**
+     * Act - do whatever the zombie wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private int x;
+    private int y;
+    
+    public void act() {
+        
+    }
+    
+    public void setx(int x) {
+        this.x = x;
+        
+    }
+    
+    public void sety(int x) {
+        this.y = y;
+        
+    }
+    
+}
