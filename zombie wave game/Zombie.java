2420bea08c1fc8e63643a3783e44601754481b92
p7gallup24 <89165529+p7gallup24@users.noreply.github.com>
2022-05-27 14:46:19 -0700

    Add files via upload

--- zombie wave game/Zombie.java
+++ zombie wave game/Zombie.java
@@ -9,15 +9,43 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Zombie extends Actor
 {
     private static int numAlive = 0;
+    private static int healthPoints = 1000;
+    private static int puddleAnim;
+    
+    public Zombie() {
+        int[] puddleAnim = {dis1.jpeg, dis2.jpeg, dis3.jpeg, dis4.jpeg, dis5.jpeg};
+        this.puddleAnim = puddleAnim;
+    }
     public void act() {
+        setLocation(getX(), getY()+1);
+        changeImage()
+        if(touching(Bullet) {
+               puddlefied();
+            
+        }
+    }
+    
+    private void changeImage() {
+        if(getImageFileName() == walking_right.jpeg) {
+            setActorImage(walking_left.jpeg);
+            
+        }else {
+            setActorImage(walking_right.jpeg);   
+        }
+        
+    }
+    
+    private void puddlefied() {
         
     }
     
     private void death() {
+        numAlive -= 1;
         
     }
     
     public void alive() {
+        numAlive += 1;
         
     }
 }
